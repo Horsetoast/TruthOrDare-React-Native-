06fa0414d756f665e5af208183e9eb5899a20e41
Matus <peciar.matus@gmail.com>
2018-08-01 21:22:01 +0200

    Splash screen / two apps issues - fixed

--- android/app/src/main/java/com/truthordare/MainApplication.java
+++ android/app/src/main/java/com/truthordare/MainApplication.java
@@ -3,8 +3,8 @@ package com.truthordare;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-// import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+// import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
